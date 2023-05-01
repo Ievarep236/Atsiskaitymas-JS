203be2a5c7a61479f9a305bb469ac342d71081f3
Ievarep236 <54540748+Ievarep236@users.noreply.github.com>
2023-05-01 15:12:02 +0300

    Fourth task done

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,3 +9,39 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+const createCards = (data) => {
+  const outputDiv = document.getElementById('output');
+
+  data.forEach((item) => {
+    const brandContainer = document.createElement('div');
+    const brand = document.createElement('h2');
+    const list = document.createElement('ul');
+    brandContainer.classList = 'brandContainer';
+    brand.classList = 'brand';
+    list.classList = 'list';
+
+    brand.textContent = item.brand;
+    item.models.forEach((modelItem) => {
+      const listitem = document.createElement('li');
+      listitem.classList = 'listitem';
+      listitem.textContent = modelItem;
+      list.append(listitem);
+    });
+    brandContainer.append(brand, list);
+    outputDiv.append(brandContainer);
+  });
+};
+
+const getData = async () => {
+  try {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    console.log(data);
+    createCards(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getData();
